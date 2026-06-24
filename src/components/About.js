5b3f2972a484c04e98d3b70c404459b21f4f8402
Riya Kumari <110655649+RiyaKumari22@users.noreply.github.com>
2026-06-24 14:28:24 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const About = () => {
 
         {/* Right Side - Description */}
         <div className="about-text">
-         <p > Riya Kumari is a Web Developer and Project Assistant – Online Management at FSR Global, where she manages websites, supports SEO and Google Analytics, and maintains digital learning platform such as Moodle and Canvas. She specializes in front-end design, troubleshooting technical issues, and ensuring smooth digital program delivery.
+         <p > Riya Kumari is a Web Developer and Program Assistant at FSR Global, where she manages websites, supports SEO and Google Analytics, and maintains digital learning platform such as Moodle and Canvas. She specializes in front-end design, troubleshooting technical issues, and ensuring smooth digital program delivery.
 <br></br>
 <br></br>
 With a strong foundation in Computer Science Engineering, Riya’s technical expertise spans HTML, CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, SQL, and WordPress. She also works with cloud platforms like Microsoft Azure and Oracle, and is skilled in Git, Postman, and digital platform optimization.
